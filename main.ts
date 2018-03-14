7009578f64d693b69dbe108648aee15f05280abc
Evan Gipson <evangipson@gmail.com>
2018-03-14 02:12:17 -0500

    Update comments, run JSHint
    Yay removing bad ECMAScript and including good comments!
    
    Also functionalized more functions! :)

--- main.ts
+++ main.ts
@@ -12,9 +12,8 @@ const waveTypes = [
     "triangle",
     "square"
 ];
-// in half steps
-const baseTone = 120;
-// root not included, top octave note note included.
+const baseTone = 120; // in Hz
+// root tone included as 0, top octave note not included.
 const scales = {
     major: [
         0,
@@ -70,7 +69,7 @@ const scales = {
         10
     ]
 };
-const twelfthRootOfTwo = 1.05946309;
+const twelfthRootOfTwo = 1.05946309; // need this to calculate Hz based on interval & scale
 let currentScale = scales.naturalMinor; // will need this later for UI
 
 // Pure Functions
@@ -83,24 +82,27 @@ const maybe = func => {
         return func;
     }
     return null;
-}
+};
 
 /**
  * Gives back a random array item, provided
-* the array.
-* @param {Array} array 
-*/
+ * the array.
+ * @param {Array} array
+ */
 const getRandomArrayItem = array => array[Math.floor(Math.random()*array.length)];
 
 /**
  * Gives back a number in the range provided.
-* @param {Number} min 
-* @param {Number} max 
-*/
+ * @param {Number} min
+ * @param {Number} max
+ */
 const getRange = (min, max) => Math.floor(Math.random() * max) + min;
 
 /**
  * Gets a unique note in the chord, not the one provided.
+ * Relies on twelfthRootOfTwo.
+ * @param {Number} note in Hz
+ * @param {Array} scale interval array
  */
 const getUniqueChordNote = (note, scale) => {
     let returnNote = getHarmonicNoteFrequency(getRandomArrayItem(scale));
@@ -112,18 +114,25 @@ const getUniqueChordNote = (note, scale) => {
     return returnNote;
 };
 
+/**
+ * Will return a gain node, given a volume less than 1.
+ * If volume is 0, null is returned.
+ * Relies on audioContext.
+ * @param {Number} volume
+ */
 const getGainNode = volume => {
-    if(volume > 1.0 && audioContext) {
-        return audioContext.createGain().
-                gain.setValueAtTime(volume, 1);
+    if(volume > 0 && volume < 1.0) {
+        const gainNode = audioContext.createGain();
+        gainNode.gain.setValueAtTime(volume, audioContext.currentTime);
+        return gainNode;
     }
     return null;
 };
 
 /**
  * Will get a note position given an event.
- * Will default to give a position somewhere inside of the visualizer.
- * @param event 
+ * Will return null if provided no event.
+ * @param {HTMLEvent} event
  */
 const getNotePosition = event => {
     return {
@@ -132,33 +141,95 @@ const getNotePosition = event => {
     };
 };
 
+/**
+ * Sets the passed in osc's type and frequency,
+ * then returns that osc.
+ * Relies on audioContext.
+ * @param {Oscillator} osc
+ * @param {String} type
+ * @param {Number} frequency in Hz
+ */
 const setOscProperties = (osc, type, frequency) => {
     osc.type = type;
-    osc.frequency.setValueAtTime(frequency, 0);
+    osc.frequency.setValueAtTime(frequency, audioContext.currentTime);
     return osc;
 };
 
 /**
- * Connects an oscillator to a destination of
-* the node passed in.
-*/
+ * Connects a new oscillator and hooks up a gain node
+ * if it needs to, then returns the connected oscillator.
+ * Relies on audioContext.
+ * @param {GainNode} gainNode
+ */
 const connectOscNode = (gainNode) => {
     const osc = audioContext.createOscillator();
     if(gainNode) {
         osc.connect(gainNode);
         gainNode.connect(audioContext.destination);
-        return gainNode;
     }
     else {
         osc.connect(audioContext.destination);
-        return osc;
     }
+    return osc;
 };
 
+/**
+ * Starts the given osc, then stops it in the
+ * time provided, in seconds.
+ * Relies on audioContext.
+ * @param {Oscillator} osc
+ * @param {Number} time in seconds
+ */
 const startOscillator = (osc, time) => {
     osc.start();
     osc.stop(audioContext.currentTime + time);
     return osc;
+};
+
+/**
+ * Will generate a frequency based on a scale.
+ * Relies on baseTone and twelfthRootOfTwo
+ * @param {Array} scale interval array
+ */
+const getHarmonicNoteFrequency = scale => {
+    let harmonicInterval = getRandomArrayItem(scale);
+    // in a 2 octave range, 1 up 1 down
+    harmonicInterval = Math.random() > 0.50 ? -(harmonicInterval) : harmonicInterval;
+    // perform our calculation to give back our frequency
+    return baseTone * Math.pow(twelfthRootOfTwo, harmonicInterval);
+};
+
+/**
+ * Will generate a chord, returned as a set filled with
+ * unique values. Relies on currentScale.
+ * @param {Number} baseNote the root note of the chord, interval
+ * @param {Number} tones how many notes you want in the chord
+ */
+const getChord = (baseNote, tones) => {
+    let chordTones = [];
+    for(var i = 0; i < tones; i++) {
+        chordTones.push(getUniqueChordNote(baseNote, currentScale));
+    }
+    // since sets can only store unique values, let's make
+    // a set with the chord tones, since i want them unique.
+    return new Set(chordTones);
+};
+
+/**
+ * Creates a click event somewhere randomly on the visualizer,
+ * within a window of the gutter provided, and returns the event.
+ * @param {Number} screenGutter how much edge of the screen to give, in px.
+ */
+const getFakeMouseClick = (screenGutter = 100) => {
+    const bestGuessX = getRange(screenGutter, document.getElementById("Visualizer").offsetWidth - screenGutter);
+    const bestGuessY = getRange(screenGutter, document.getElementById("Visualizer").offsetHeight - screenGutter);
+    return new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+        'clientX': bestGuessX,
+        'clientY': bestGuessY
+    });
 };
 
 // Non-Pure Functions
@@ -168,19 +239,19 @@ const startOscillator = (osc, time) => {
 * @param {String} type of waveform. default is sine.
 * @param {Number} frequency in Hz. default is 440.
 * @param {Number} time in seconds. default is .5s.
-* @param {Number} volume from 0 to 1. default is 1.0.
-* @param {Number} echoDelay in milliseconds. default is false.
-* @param {HTMLEvent} event to give us x and y
+* @param {Number} volume from 0 to 1. default is 1.0, so 100%.
+* @param {Number} echoDelay used with notes that have echo, in milliseconds. default is false.
+* @param {HTMLEvent} event to determine where to draw the circles. default is null.
 */
-function playMIDINote(type = "sine", frequency = 440, time = 0.5, volume = 1.0, echoDelay = false, event) {
+function playMIDINote(type = "sine", frequency = 440, time = 0.5, volume = 1.0, echoDelay = false, event = null) {
     // handle creating an oscillator and starting & stopping it
     const osc = startOscillator(
                 setOscProperties(
-                    connectOscNode(getGainNode(volume)),type,frequency
+                    connectOscNode(getGainNode(volume)), type, frequency
                 ), time);
     // draw those pretty circles on the canvas
     if(event) {
-        drawNote(echoDelay, event, volume);
+        drawNoteWithVolumeBasedOpacity(echoDelay, event, volume);
     }
     // handle repeating notes
     if(echoDelay && volume > 0.2) {
@@ -191,24 +262,18 @@ function playMIDINote(type = "sine", frequency = 440, time = 0.5, volume = 1.0, 
     }
 }
 
-// Not pure because it's random
+/**
+ * Will return a random note duration, in seconds.
+ * Note: Isn't pure because it's random, and has no inputs.
+ */
 function getRandomNoteDuration() {
     return Math.random() * 0.4 + 0.08;
 }
 
 /**
- * Will generate a frequency based on a scale.
- * Relies on baseTone and twelfthRootOfTwo
- * @param scale an array of intervals.
+ * Will return an object containing properties
+ * defining a "pad", or long, usually background note.
  */
-const getHarmonicNoteFrequency = scale => {
-    let harmonicInterval = getRandomArrayItem(scale);
-    // in a 2 octave range, 1 up 1 down
-    harmonicInterval = Math.random() > 0.50 ? -(harmonicInterval) : harmonicInterval;
-    // perform our calculation to give back our frequency
-    return baseTone * (twelfthRootOfTwo ^ harmonicInterval);
-};
-
 function assemblePadNote() {
     return {
         type: Math.random() > 0.50 ? "triangle" : "sine",
@@ -216,9 +281,13 @@ function assemblePadNote() {
         time: getRandomNoteDuration() * getRange(3, 10),
         echoDelay: null,
         volume: getRange(0.4, 0.7)
-    }
+    };
 }
 
+/**
+ * Will return an object containing properties
+ * defining a "normal" note.
+ */
 function assembleNormalNote() {
     return {
         type: getRandomArrayItem(waveTypes),
@@ -226,10 +295,18 @@ function assembleNormalNote() {
         time: getRandomNoteDuration(),
         volume: 1.0,
         echoDelay: maybe(getRange(200, 2000)), // in ms
-    }
+    };
 }
 
-function drawNote(echoDelay, event, volume) {
+/**
+ * Will draw the expanding circle factoring in volume
+ * as opacity (out of 1.0). Also handles getting the note
+ * position from the event.
+ * @param {Number} echoDelay
+ * @param {HTMLEvent} event
+ * @param {Number} volume
+ */
+function drawNoteWithVolumeBasedOpacity(echoDelay, event, volume) {
     let coords = getNotePosition(event);
     if(echoDelay) {
         drawNoteCircle(coords.x, coords.y, volume);
@@ -239,6 +316,11 @@ function drawNote(echoDelay, event, volume) {
     }
 }
 
+/**
+ * The function responsible for playing a MIDI note
+ * after you've clicked the mouse.
+ * @param {HTMLEvent} event
+ */
 function playNoteOnClick(event) {
     /* as long as we provide an echoDelay, we'll
     * hear an echo. */
@@ -254,39 +336,8 @@ function playNoteOnClick(event) {
 }
 
 /**
- * Clicks somewhere randomly on the visualizer, within a window.
- * @param {Number} screenGutter how much edge of the screen to give, in px.
- */
-function fakeMouseClick(screenGutter = 100) {
-    const bestGuessX = getRange(screenGutter, document.getElementById("Visualizer").offsetWidth - screenGutter);
-    const bestGuessY = getRange(screenGutter, document.getElementById("Visualizer").offsetHeight - screenGutter);
-    return new MouseEvent('click', {
-        'view': window,
-        'bubbles': true,
-        'cancelable': true,
-        'clientX': bestGuessX,
-        'clientY': bestGuessY
-    });
-}
-
-/**
- * Will generate a chord, returned as a set filled with
- * unique values. Relies on currentScale.
- * @param baseNote the root note of the chord
- * @param tones how many notes you want in the chord
- */
-const getChord = (baseNote, tones) => {
-    let chordTones = [];
-    for(var i = 0; i < tones; i++) {
-        chordTones.push(getUniqueChordNote(baseNote, currentScale));
-    }
-    // since sets can only store unique values, let's make
-    // a set with the chord tones, since i want them unique.
-    return new Set(chordTones);
-}
-
-/**
  * "Starts" the radio and keeps it going by calling itself.
+ * This is the driver of the automatic radio generation.
  * Note: Can generate either a tone, echo tone, or chord.
  */
 function generateSound() {
@@ -298,7 +349,7 @@ function generateSound() {
         note.time,
         note.volume,
         note.echoDelay,
-        fakeMouseClick()
+        getFakeMouseClick()
     );
     // take care of chords if there is one.
     getChord(note, additionalChordTones).forEach((chordTone) => {
@@ -308,17 +359,20 @@ function generateSound() {
             chordTone.time,
             chordTone.volume,
             false, // no echo for chords
-            fakeMouseClick()
+            getFakeMouseClick()
         );
     });
 
     // now in a random amount of time, call itself again.
     const msUntilNextNote = getRange(0.25, 5) * 1000; // in ms
     window.setTimeout(function() {
-        generateSound()
+        generateSound();
     }, msUntilNextNote);
 }
 
+/**
+ * Wires up the close/open functionality of the controls menu.
+ */
 function enableControlMenu() {
     let showControlsButton = document.getElementById("ShowControls");
     let content = document.getElementById("ControlList");
@@ -335,6 +389,13 @@ function enableControlMenu() {
     });
 }
 
+/**
+ * Takes care of drawing the expanding notes on screen.
+ * Intended to be called by playMIDINote() so shouldn't be called directly.
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} opacity
+ */
 function drawNoteCircle(x, y, opacity = "1.0") {
     let newCircle = document.createElement("span");
     newCircle.classList.add("note-circle");
